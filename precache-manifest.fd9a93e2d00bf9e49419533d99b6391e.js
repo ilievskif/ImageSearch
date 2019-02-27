a927fd3b0984421de2667d73863e20bdd1049fe6
Filip <filip.ilievski96@gmail.com>
2019-02-27 17:38:21 +0100

    Updates

---
+++ precache-manifest.fd9a93e2d00bf9e49419533d99b6391e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f4b79439199fab0e2f3",
+    "url": "/ImageSearch/static/css/main.2aa27b69.chunk.css"
+  },
+  {
+    "revision": "1f4b79439199fab0e2f3",
+    "url": "/ImageSearch/static/js/main.1f4b7943.chunk.js"
+  },
+  {
+    "revision": "8e86e34f75e88effe6a4",
+    "url": "/ImageSearch/static/js/runtime~main.8e86e34f.js"
+  },
+  {
+    "revision": "cb016095e848669d37ac",
+    "url": "/ImageSearch/static/js/2.cb016095.chunk.js"
+  },
+  {
+    "revision": "d48632e0f82ef5e7cb517719b872e437",
+    "url": "/ImageSearch/index.html"
+  }
+];
